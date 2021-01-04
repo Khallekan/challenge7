2543afde0e12e1880accd3b41f99eedd26d324d3
Khallekan <okeolalekanisaac@gmail.com>
2021-01-04 11:45:11 +0100

    Now responsive

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form');
 const input = document.getElementById('email');
 const alert = document.getElementById('alert');
+const error = document.getElementById('error');
+
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -9,18 +11,21 @@ function validateEmail(email) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    error.style.display = 'none';
     alert.innerHTML = "";
     let inputValue = input.value.trim();
     let result = validateEmail(inputValue);
 
     if(result) {
         console.log("valid");
+        input.value = ""
         alert.insertAdjacentHTML(
             `beforeend`,
             `<p class="alert1">Thank you for signing up. We will get back to you shortly</p>`
         )
     } else {
         console.log('invalid');
+        error.style.display = 'block';
         alert.insertAdjacentHTML(
             `beforeend`,
             `<p class="alert">Please provide a valid email</p>`
